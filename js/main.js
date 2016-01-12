a7f2d3efdd06df552ec2e431038ccc86b717b5ae
allen0346 <allen0346@gmail.com>
2016-01-12 17:03:37 -0600

    add mobile-view_menu_navBar_mainContent

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@
 })(jQuery);
 
 new WOW().init();
+
+(function($){
+  $("#mobile-icon").click(function(){
+    // console.log($("nav").css("top"));
+    if($("nav").css("top") =="-100px"){
+      $(this).animate({top: "60px"}, 300 );
+      $("nav").animate({top: "0"},300);
+    }else{
+      $(this).animate({top: "-50px"}, 300 );
+      $("nav").animate({top: "-100px"},300);
+    }
+  });
+}(jQuery));
